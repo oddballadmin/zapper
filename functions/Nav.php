bfb73477bc036cf51cb7f8879d19f728514f0ea8
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2023-01-21 10:47:14 -0500

    added getters and setters/ nav functionality

---
+++ functions/Nav.php
@@ -0,0 +1,60 @@
+<?php
+
+class Nav {
+    // name of Nav bar
+    public $name;
+    // checks if user is Logged in
+    public $logInState = [0,1];
+    // gets current page that the user is currently on
+    public $currentPage;
+
+    // Getters and setters for Nav Class
+    function setName($name){
+        $this->name = $name;
+    }
+    function getName(){
+        return $this-> name;
+    }
+    function setLogInState($logInState){
+        $this->logInState = $logInState;
+    }
+    function getLogInState(){
+        return $this->logInState;
+    }
+    function setCurrentPage($currentPage){
+        $this->currentPage = $currentPage;
+    }
+    function getCurrentPage(){
+        return $this->currentPage;
+    }
+
+
+    // creates initial nav opening
+    function createNav(){
+        $navContents = "";
+        $navContent =+ "<nav>";
+        $isUserGuest = true;
+        switch($this->logInState){
+            case 0: 
+                $isUserGuest = true;
+                $navContent =+ "<section id=\"user-account\"></section>";
+                break;
+            case 1:
+                $isUserGuest = false;
+                "<section id=\"guest-account\"></section>";
+                break;
+        }
+        
+    }
+    function removeNav(){
+
+    }
+    function addItemtoNavList(){
+
+    }
+    function removeItemToNavList(){
+    }
+    function addNavToDom($navName){
+
+    }
+}
